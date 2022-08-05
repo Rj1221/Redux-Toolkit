2ef009efb26dddd9fc679b1480da8e252b752cfb
Raj <rj6207491172@gmail.com>
2022-08-05 09:02:37 +0530

    Async Action using Redux Thunk in Action Creater

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -7,4 +7,12 @@ export const updateStatus = ( status ) => ( {
 // New Concept of Redux toolkit
 import { createAction } from '@reduxjs/toolkit';
 
+// Using Redux Toolkit createAction to create action for normal request not for async request
 export const updateStatus = createAction('UPDATE_STATUS');
+
+// Using Redux Toolkit createAction to create action for async request
+export const fetchName = () => async (dispatch) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const result = await res.json();
+  dispatch({ type: 'UPDATE_NAME', payload: result[0].name });
+};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateStatus as update } from '../../action/userAction';
+import { updateStatus as update, fetchName } from '../../action/userAction';
 
 const Profile = () => {
   const { name: userName, age: userAge, status: userStatus } = useSelector((state) => state.user);
@@ -8,8 +8,12 @@ const Profile = () => {
   const updateAge = (age) => {
     dispatch({ type: 'UPDATE_AGE', payload: age });
   };
-  const updateName = (name) => {
-    dispatch({ type: 'UPDATE_NAME', payload: name });
+  const updateName = async () => {
+    // We can do async stuff here but its not a proper way we should do it in action
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    // const result = await res.json();
+    // dispatch({ type: 'UPDATE_NAME', payload: result[0].name });
+    dispatch(fetchName());
   };
   const updateStatus = (status) => {
     dispatch(update(status));
@@ -22,7 +26,7 @@ const Profile = () => {
       <p>Status: { userStatus }</p>
       <div>
         <button type="button" onClick={() => updateAge(40, 'RajuRastogi')}>Update Age</button>
-        <button type="button" onClick={() => updateName('RajuRastogi')}>Update Name</button>
+        <button type="button" onClick={() => updateName()}>Update Name</button>
         <button type="button" onClick={() => updateStatus('Programmer')}>Update Status</button>
       </div>
     </div>
